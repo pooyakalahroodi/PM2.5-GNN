4728ae20a40b9c896ab44e95063343cc48c053bd
pooya kalahroodi <pooyakalahroodi@gmail.com>
2023-09-01 19:43:30 +0200

    Create TransformerGNN.py

---
+++ model/TransformerGNN.py
@@ -0,0 +1,47 @@
+from transformer import CustomTransformer  # Import your Transformer module
+
+class PM25_GNN(nn.Module):
+    def __init__(self, hist_len, pred_len, in_dim, city_num, batch_size, device, edge_index, edge_attr, wind_mean, wind_std):
+        super(PM25_GNN, self).__init__()
+
+        self.device = device
+        self.hist_len = hist_len
+        self.pred_len = pred_len
+        self.city_num = city_num
+        self.batch_size = batch_size
+
+        self.in_dim = in_dim
+        self.hid_dim = 64
+        self.out_dim = 1
+        self.gnn_out = 13
+
+        self.fc_in = nn.Linear(self.in_dim, self.hid_dim)
+        self.graph_gnn = GraphGNN(self.device, edge_index, edge_attr, self.in_dim, self.gnn_out, wind_mean, wind_std)
+        
+        # Replace the GRU cell with the CustomTransformer module
+        self.transformer = CustomTransformer(input_dim=self.in_dim + self.gnn_out, hidden_dim=self.hid_dim, num_heads=4, num_layers=2)
+        
+        self.fc_out = nn.Linear(self.hid_dim, self.out_dim)
+
+    def forward(self, pm25_hist, feature):
+        pm25_pred = []
+        h0 = torch.zeros(self.batch_size * self.city_num, self.hid_dim).to(self.device)
+        hn = h0
+        xn = pm25_hist[:, -1]
+        for i in range(self.pred_len):
+            x = torch.cat((xn, feature[:, self.hist_len + i]), dim=-1)
+
+            xn_gnn = x
+            xn_gnn = xn_gnn.contiguous()
+            xn_gnn = self.graph_gnn(xn_gnn)
+            x = torch.cat([xn_gnn, x], dim=-1)
+
+            # Use the Transformer module instead of GRU
+            hn = self.transformer(xn)  # You may need to adjust this part
+            xn = hn.view(self.batch_size, self.city_num, self.hid_dim)
+            xn = self.fc_out(xn)
+            pm25_pred.append(xn)
+
+        pm25_pred = torch.stack(pm25_pred, dim=1)
+
+        return pm25_pred
